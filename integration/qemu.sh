dc09ae3094902459adfefd91562371846be2cdaa
Niels Möller <nisse@glasklarteknik.se>
2023-10-23 15:18:34 +0200

    refactor: Simplify qemu test script
    Change directory to the location of the script, so files can be
    referenced relative to current directory. Override GOBIN, not GOPATH.
    Use basenc from coreutils for hex decoding, eliminating a dependency.

--- integration/qemu.sh
+++ integration/qemu.sh
@@ -10,8 +10,10 @@
 set -eu
 trap clean_up EXIT
 
-BUILD=$(realpath "$(dirname "$0")"/build)
-SAVED=$(realpath "$(dirname "$0")"/saved)
+# Change directory to where script is located.
+cd $(dirname $0)
+# Use local directory for built go tools.
+export GOBIN="$(pwd)"/bin
 
 qemu_pid=""
 function clean_up() {
@@ -19,26 +21,27 @@ function clean_up() {
   ps -p $qemu_pid > /dev/null && kill $qemu_pid
 }
 
-mkdir -p "$BUILD" "$SAVED"
+mkdir -p build saved
 
 ###
 # Build
 ###
-GOPATH="$BUILD"/go go install ../cmd/stprov
-[[ -d "$BUILD"/u-root ]] ||
-  git clone --depth 1 https://github.com/u-root/u-root "$BUILD"/u-root &&
-  pushd "$BUILD"/u-root && GOPATH="$BUILD"/go go install && popd
+
+go install ../cmd/stprov
+[[ -d build/u-root ]] ||
+  git clone --depth 1 https://github.com/u-root/u-root build/u-root &&
+  (cd build/u-root && go install)
 
 url="https://git.glasklar.is/system-transparency/core/system-transparency/-/raw/main/contrib/linuxboot.vmlinuz"
-[[ -f "$BUILD"/kernel.vmlinuz ]] || curl -L "$url" -o "$BUILD"/kernel.vmlinuz
+[[ -f build/kernel.vmlinuz ]] || curl -L "$url" -o build/kernel.vmlinuz
 
-"$BUILD"/go/bin/u-root\
-  -o "$BUILD"/stprov.cpio\
-  -uroot-source="$BUILD"/u-root\
+./bin/u-root\
+  -o build/stprov.cpio\
+  -uroot-source=build/u-root\
   -uinitcmd="/bin/sh /bin/uinitcmd.sh"\
-  -files "$BUILD"/go/bin/stprov:bin/stprov\
+  -files bin/stprov:bin/stprov\
   -files uinitcmd.sh:bin/uinitcmd.sh\
-  "$BUILD"/u-root/cmds/core/{init,elvish}
+  build/u-root/cmds/core/{init,elvish}
 
 echo "PASS: build"
 
@@ -53,7 +56,7 @@ for str in "OVMF" "edk2/ovmf" "edk2-ovmf/x64"; do
   file=/usr/share/"$str"/OVMF_CODE.fd
   if [[ -f "$file" ]]; then
     ovmf_code="$file"
-    cp /usr/share/"$str"/OVMF_VARS.fd "$SAVED"/OVMF_VARS.fd
+    cp /usr/share/"$str"/OVMF_VARS.fd saved/OVMF_VARS.fd
     break
   fi
 done
@@ -74,10 +77,10 @@ qemu-system-x86_64 -nographic\
   -object rng-random,filename=/dev/urandom,id=rng0\
   -device virtio-rng-pci,rng=rng0\
   -drive if=pflash,format=raw,readonly=on,file="$ovmf_code"\
-  -drive if=pflash,format=raw,file="$SAVED"/OVMF_VARS.fd\
-  -kernel "$BUILD"/kernel.vmlinuz\
-  -initrd "$BUILD"/stprov.cpio\
-  -append "console=ttyS0" >"$SAVED"/qemu.log &
+  -drive if=pflash,format=raw,file=saved/OVMF_VARS.fd\
+  -kernel build/kernel.vmlinuz\
+  -initrd build/stprov.cpio\
+  -append "console=ttyS0" >saved/qemu.log &
 qemu_pid=$!
 
 ###
@@ -91,7 +94,7 @@ function reach_stage() {
       exit 1
     fi
 
-    if [[ ! -z $(grep "$2" "$SAVED"/qemu.log) ]]; then
+    if [[ ! -z $(grep "$2" saved/qemu.log) ]]; then
       echo "PASS: reach $2" >&2
       break
     fi
@@ -103,31 +106,31 @@ function reach_stage() {
 
 reach_stage 10 "stage:boot"
 reach_stage 60 "stage:network"
-"$BUILD"/go/bin/stprov local run --ip 127.0.0.1 --otp sikritpassword | tee "$SAVED"/stprov.log
+./bin/stprov local run --ip 127.0.0.1 --otp sikritpassword | tee saved/stprov.log
 reach_stage 3 "stage:shutdown"
 
-hostname=$(grep hostname "$SAVED"/stprov.log | cut -d'=' -f2)
-fingerprint=$(grep fingerprint "$SAVED"/stprov.log | cut -d'=' -f2)
-virt-fw-vars -i "$SAVED"/OVMF_VARS.fd --output-json "$SAVED"/efivars.json
+hostname=$(grep hostname saved/stprov.log | cut -d'=' -f2)
+fingerprint=$(grep fingerprint saved/stprov.log | cut -d'=' -f2)
+virt-fw-vars -i saved/OVMF_VARS.fd --output-json saved/efivars.json
 
-got=$(cat "$SAVED"/efivars.json | jq -r '.variables[] | select(.name == "STHostName") | .data' | base16 -d)
+got=$(cat saved/efivars.json | jq -r '.variables[] | select(.name == "STHostName") | .data' | tr a-f A-F | basenc --base16 -d)
 if [[ "$got" != "$hostname" ]]; then
   echo "FAIL: wrong hostname in EFI NVRAM ($got)" >&2
   exit 1
 fi
 echo "PASS: EFI-NVRAM hostname"
 
-cat "$SAVED"/efivars.json | jq -r '.variables[] | select(.name == "STHostKey") | .data' | base16 -d > "$SAVED"/hostkey
-chmod 600 "$SAVED"/hostkey
-got=$(ssh-keygen -lf "$SAVED"/hostkey | cut -d' ' -f2)
+cat saved/efivars.json | jq -r '.variables[] | select(.name == "STHostKey") | .data' | tr a-f A-F | basenc --base16 -d > saved/hostkey
+chmod 600 saved/hostkey
+got=$(ssh-keygen -lf saved/hostkey | cut -d' ' -f2)
 if [[ "$got" != "$fingerprint" ]]; then
   echo "FAIL: wrong fingerprint for key in EFI NVRAM ($got)" >&2
   exit 1
 fi
 echo "PASS: EFI-NVRAM hostkey"
 
-cat "$SAVED"/efivars.json | jq -r '.variables[] | select(.name == "STHostConfig") | .data' | base16 -d | jq > "$SAVED"/hostcfg.json
-got=$(cat "$SAVED"/hostcfg.json | jq '.ospkg_pointer')
+cat saved/efivars.json | jq -r '.variables[] | select(.name == "STHostConfig") | .data' | tr a-f A-F | basenc --base16 -d | jq > saved/hostcfg.json
+got=$(cat saved/hostcfg.json | jq '.ospkg_pointer')
 if [[ "$got" != "\"https://example.org/ospkg.json\"" ]]; then
   echo "FAIL: wrong URL in EFI NVRAM host config ($got)" >&2
   exit 1
