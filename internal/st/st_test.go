cb8c66fb9a0be5da0ec7b1a5915977032a83cc38
Rasmus Dahlberg <rgdd@glasklarteknik.se>
2024-01-18 10:45:45 +0100

    test: Drop read/write host config efi tests
    We used to have separate setup functions to produce and test valid host
    configurations for different configurations: static, dhcp, and bonding.
    The idea for them was: have something smaller that we can unit test.
    
    Exactly how good these units tests were is a separate question.  Anyway.
    
    Over time, these setup functions were not maintained.  Configuration
    options were instead added directly in "main", and the logic to do a
    correct setup are spread out all over the place.  As of the recent move
    to using stboot's implementation of host configuration, these setup
    functions were deemed obsolete and have been removed.  This commit also
    removes the tests that were intended for these isolated setup functions.
    
    Note that the internal reading and writing of EFI NVRAM is still being
    tested separately from the removed unit test.  And that our qemu tests
    try the path of json (un)marshalling with stboot's host configuration
    package, then reading/writing with our internal EFI NVRAM reads/writes.
    So in terms of actual test coverage we havn't really removed anything.
    
    As an aside, the deleted tests did not run anymore.  The motivation for
    deleting rather than fixing is (again) because they didn't add anything.
    
    (What stprov really needs is refactoring to be more testable.  And this
    is not the right time to get started with that type of refactoring.)

--- internal/st/st_test.go
+++ internal/st/st_test.go
@@ -1,39 +1,11 @@
 package st
 
 import (
-	"net"
 	"os"
-	"reflect"
 	"testing"
-	"time"
 
 	"github.com/google/uuid"
-	"system-transparency.org/stboot/host"
-	"system-transparency.org/stprov/subcmd/remote/dhcp"
-	"system-transparency.org/stprov/subcmd/remote/static"
 )
-
-func TestReadWriteEFI(t *testing.T) {
-	if os.Getenv("TEST_CLOBBER_EFI_NVRAM") == "" {
-		t.Skip("Skipping tests associated with TEST_CLOBBER_EFI_NVRAM")
-	}
-
-	cfg1, _ := static.Config(nil, "1.1.1.1", "eth0", "192.168.0.2/32", "192.168.0.1", 10*time.Second, true, false, nil, "", true, false)
-	cfg2, _ := dhcp.Config(nil, "2.2.2.2", "eth0", 10*time.Second, true)
-	for i, cfg := range []*host.Config{cfg1, cfg2} {
-		if err := WriteHostConfigEFI(cfg); err != nil {
-			t.Errorf("%d: %v", i, err)
-			return
-		}
-		cfgAgain, err := HostConfigEFI()
-		if err != nil {
-			t.Errorf("%d: %v", i, err)
-		}
-		if got, want := &cfgAgain, cfg; !reflect.DeepEqual(got, want) {
-			t.Errorf("%d: got config\n%v\nbut wanted\n%v", i, got, want)
-		}
-	}
-}
 
 func TestReadWriteHostName(t *testing.T) {
 	if os.Getenv("TEST_CLOBBER_EFI_NVRAM") == "" {
@@ -62,12 +34,4 @@ func testUUID(t *testing.T) *uuid.UUID {
 		t.Fatal(err)
 	}
 	return &varUUID
-}
-
-func testHardwareAddr(t *testing.T) *net.HardwareAddr {
-	a, err := net.ParseMAC("aa:aa:aa:bb:bb:bb")
-	if err != nil {
-		t.Fatal(err)
-	}
-	return &a
 }
