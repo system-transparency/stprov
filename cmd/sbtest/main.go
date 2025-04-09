905084983130a75c2f5ed1c17df6e37c88b87083
Rasmus Dahlberg <rgdd@glasklarteknik.se>
2025-04-09 21:09:45 +0200

    x

--- cmd/sbtest/main.go
+++ cmd/sbtest/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 
-	"system-transparency.org/stprov/internal/api"
 	"system-transparency.org/stprov/internal/sb"
 )
 
@@ -15,27 +14,63 @@ var (
 )
 
 func main() {
-	pk, err := sb.ReadOptionalESLFile(optPKFile)
-	if err != nil {
-		log.Fatalf("invalid pk: %v\n", err)
-	}
-	kek, err := sb.ReadOptionalESLFile(optKEKFile)
-	if err != nil {
-		log.Fatalf("invalid kek: %v\n", err)
-	}
-	db, err := sb.ReadOptionalESLFile(optDBFile)
-	if err != nil {
-		log.Fatalf("invalid db: %v\n", err)
-	}
-	dbx, err := sb.ReadOptionalESLFile(optDBXFile)
-	if err != nil {
-		log.Fatalf("invalid dbx: %v\n", err)
-	}
+	//pk, err := sb.ReadOptionalESLFile(optPKFile)
+	//if err != nil {
+	//	log.Fatalf("invalid pk: %v\n", err)
+	//}
+	//kek, err := sb.ReadOptionalESLFile(optKEKFile)
+	//if err != nil {
+	//	log.Fatalf("invalid kek: %v\n", err)
+	//}
+	//db, err := sb.ReadOptionalESLFile(optDBFile)
+	//if err != nil {
+	//	log.Fatalf("invalid db: %v\n", err)
+	//}
+	//dbx, err := sb.ReadOptionalESLFile(optDBXFile)
+	//if err != nil {
+	//	log.Fatalf("invalid dbx: %v\n", err)
+	//}
+	//req, _ := api.NewAddSecureBootRequest(pk, kek, db, dbx)
 
-	req, _ := api.NewAddSecureBootRequest(pk, kek, db, dbx)
+	log.Printf("*** Before provisioning\n")
+	status()
+
 	if err := sb.ProvisionKeys(req.PK, req.KEK, req.DB, req.DBX); err != nil {
 		log.Fatalf("provision keys: %v\n", err)
 	}
+	if err := sb.SetSecureBoot(); err != nil {
+		log.Fatalf("set SecureBoot: %v\n", err)
+	}
+	if err := sb.SetDeployedMode(); err != nil {
+		log.Fatalf("set DeployedMode: %v\n", err)
+	}
+
+	log.Printf("*** After provisioning provisioning\n")
+	status()
 
 	log.Printf("OK\n")
+}
+
+func status() {
+	isSetupMode, err := sb.IsSetupMode()
+	if err != nil {
+		log.Fatalf("is setup mode: %v\n", err)
+	}
+	isAuditMode, err := sb.IsAuditMode()
+	if err != nil {
+		log.Fatalf("is audit mode: %v\n", err)
+	}
+	isDeployedMode, err := sb.IsDeployedMode()
+	if err != nil {
+		log.Fatalf("is deployed mode: %v\n", err)
+	}
+	isSecureBoot, err := sb.IsSetupMode()
+	if err != nil {
+		log.Fatalf("is setup mode: %v\n", err)
+	}
+
+	log.Printf("SetupMode:    %v", isSetupMode)
+	log.Printf("AuditMode:    %v", isAuditMode)
+	log.Printf("DeployedMode: %v", isDeployedMode)
+	log.Printf("SecureBoot:   %v", isSecureBoot)
 }
--- internal/sb/sb.go
+++ internal/sb/sb.go
@@ -14,22 +14,100 @@ import (
 	"github.com/foxboron/go-uefi/efi"
 	"github.com/foxboron/go-uefi/efi/signature"
 	"github.com/foxboron/go-uefi/efivar"
+	"github.com/google/uuid"
+	"github.com/u-root/u-root/pkg/efivarfs"
 )
 
-func IsSetupMode() bool {
-	return false // TODO
+func IsSetupMode() (bool, error) {
+	varUUID, _ := uuid.Parse("8be4df61-93ca-11d2-aa0d-00e098032b8c")
+	b, err := readEFI(&varUUID, "SetupMode")
+	if err != nil {
+		return false, err
+	}
+	if len(b) != 1 {
+		return false, fmt.Errorf("unexpected length: %w", err)
+	}
+	if b[0] != 0 && b[0] != 1 {
+		return false, fmt.Errorf("unexpected value: %d", b[0])
+	}
+	return b[0] == 1, nil
 }
 
-func IsDeployedMode() bool {
-	return false // TODO
+func IsDeployedMode() (bool, error) {
+	varUUID, _ := uuid.Parse("8be4df61-93ca-11d2-aa0d-00e098032b8c")
+	b, err := readEFI(&varUUID, "DeployedMode")
+	if err != nil {
+		return false, err
+	}
+	if len(b) != 1 {
+		return false, fmt.Errorf("unexpected length: %w", err)
+	}
+	if b[0] != 0 && b[0] != 1 {
+		return false, fmt.Errorf("unexpected value: %d", b[0])
+	}
+	return b[0] == 1, nil
 }
 
-func IsAuditMode() bool {
-	return false // TODO
+func IsAuditMode() (bool, error) {
+	varUUID, _ := uuid.Parse("8be4df61-93ca-11d2-aa0d-00e098032b8c")
+	b, err := readEFI(&varUUID, "AuditMode")
+	if err != nil {
+		return false, err
+	}
+	if len(b) != 1 {
+		return false, fmt.Errorf("unexpected length: %w", err)
+	}
+	if b[0] != 0 && b[0] != 1 {
+		return false, fmt.Errorf("unexpected value: %d", b[0])
+	}
+	return b[0] == 1, nil
 }
 
-func IsSecureBoot() bool {
-	return false // TODO
+func IsSecureBoot() (bool, error) {
+	varUUID, _ := uuid.Parse("8be4df61-93ca-11d2-aa0d-00e098032b8c")
+	b, err := readEFI(&varUUID, "SecureBoot")
+	if err != nil {
+		return false, err
+	}
+	if len(b) != 1 {
+		return false, fmt.Errorf("unexpected length: %w", err)
+	}
+	if b[0] != 0 && b[0] != 1 {
+		return false, fmt.Errorf("unexpected value: %d", b[0])
+	}
+	return b[0] == 1, nil
+}
+
+func SetDeployedMode() error {
+	varUUID, _ := uuid.Parse("8be4df61-93ca-11d2-aa0d-00e098032b8c")
+	return writeEFI([]byte{1}, &varUUID, "DeployedMode")
+}
+
+func SetSecureBoot() error {
+	varUUID, _ := uuid.Parse("8be4df61-93ca-11d2-aa0d-00e098032b8c")
+	return writeEFI([]byte{1}, &varUUID, "SecureBoot")
+}
+
+func writeEFI(b []byte, varUUID *uuid.UUID, efiName string) error {
+	desc := efivarfs.VariableDescriptor{Name: efiName, GUID: *varUUID}
+	attrs := efivarfs.AttributeBootserviceAccess
+	attrs |= efivarfs.AttributeRuntimeAccess
+	attrs |= efivarfs.AttributeNonVolatile
+	e, err := efivarfs.New()
+	if err != nil {
+		return fmt.Errorf("marshal: %w", err)
+	}
+	return efivarfs.WriteVariable(e, desc, attrs, b)
+}
+
+func readEFI(varUUID *uuid.UUID, efiName string) ([]byte, error) {
+	desc := efivarfs.VariableDescriptor{Name: efiName, GUID: *varUUID}
+	e, err := efivarfs.New()
+	if err != nil {
+		return nil, fmt.Errorf("marshal: %w", err)
+	}
+	_, b, err := efivarfs.ReadVariable(e, desc)
+	return b, err
 }
 
 func ReadOptionalESLFile(filename string) (*signature.SignatureDatabase, error) {
